71307db13e0209ccb51441070a8705c13d35219b
Aqeel <aqeelsaddique83@gmail.com>
2023-03-05 18:22:49 +0500

    dashboard

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
+const app = require('./app');
 
-var express = require('express');
-var app = express();
-const mongoose = require('mongoose');
 
-// DB Connection
-const db = process.env.MONGODB_URI || 'mongodb+srv://DBCONNECTION:Nvl4Z6lAb1TSAS04@cluster0.pc11gch.mongodb.net/DBCONNECTION?retryWrites=true&w=majority';
-
-// Connect to MongoDB instance
-mongoose
-  .connect(db, {
-   useNewUrlParser: true, 
-
-   useUnifiedTopology: true 
-  })
-  .then(() => console.log('MongoDB connected successfully.'))
-  .catch(err => console.log('MongoDB connection error: ' + err));
-
-const port = process.env.PORT || 4002;
-
-app.listen(port, () => console.log(`Server started on port: ${port}`));
+const dotenv = require('dotenv');
+const connectDatabase = require('./DB/database')
+//////////////handling uncaught expection error\//
+process.on("uncaughtExpection" , (err) => {
+   console.log(`Error: ${err.message}`);
+   console.log(`Shutting down the server due to uncaught expection`);
+   process.exit(1)
+})
+//config
+dotenv.config({path:"DB/config.env"});
+// Connecting to database
+// port=6780
+connectDatabase()
+ app.listen(process.env.PORT,()=>{
+    console.log(`Server is working on http://localhost:${process.env.PORT}`)
+ })
+//unhandled promise rejection
+process.on("unhandledrejection" , (err) => {
+   console.log(`error:${err.message}`);
+   console.log(`shutting down the server due to unhandled promise rejection`);
+   server.close(() => {
+      process.exit(1)
+   });
+});
